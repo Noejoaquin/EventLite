962371704482d19ed10931b804c2fe606e81a27c
Noe <noe.joaquin.james@gmail.com>
2017-10-24 12:28:57 -0400

    finish user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   validates :email, :password_digest, :session_token, presence: true, uniqueness: true
-  validates :password, length: { minimum: 6, allow_nil: true}
+  validates :password, length: { minimum: 6, allow_nil: true }
 
-  after_initialize :ensure_session_token
+  after_initialize :ensure_session_token!
 
   attr_reader :password
 
@@ -14,10 +14,21 @@ class User < ApplicationRecord
 
   def password=(password)
     @password = password
-    
-
+    self.password_digest = BCrypt::create(password)
   end
 
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token!
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
 
 end
