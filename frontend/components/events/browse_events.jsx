b34eafc0e1966996ace8ba01fa3f1c0ec98c389f
Noe <noe.joaquin.james@gmail.com>
2017-11-30 15:35:48 -0500

    get browse events page

--- frontend/components/events/browse_events.jsx
+++ frontend/components/events/browse_events.jsx
@@ -7,25 +7,18 @@ class BrowseEvents extends React.Component {
     super(props);
   }
 
-  // componentWillMount(){
-  //   if (this.props.query === null){
-  //     this.props.fetchEvents()
-  //   } else {
-  //     
-  //     this.props.fetchEvents(
-  //       {
-  //         name:this.props.query.searchBar,
-  //         category: this.props.query.searchBar,
-  //         location: this.props.query.location,
-  //         dates: this.props.query.dates
-  //       }
-  //     )
-  //   }
-  // }
-
   render(){
     return (
       <div>
+        <div className='browse-events-header-container'>
+          <div>
+            <h3 className='browse-events-header'>Things to do</h3>
+              <div className='browse-events-text'>
+                There are so many things to explore and do. Go square dancing on Pilgrim avenue. See American Jim live.
+                Get active and plant things at the Blackburn Community Garden. Get your costumed-groove on at the Alexis Charity Ball!
+              </div>
+          </div>
+        </div>
         <EventSearchContainer ownProps={this.props.ownProps}/>
       </div>
     )
