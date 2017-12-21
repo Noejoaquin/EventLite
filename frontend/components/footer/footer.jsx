8d370f9b7c2d401ad5e94d657b896162f7195d41
Noe <noe.joaquin.james@gmail.com>
2017-12-21 17:17:06 -0500

    get basic styling for footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -8,9 +8,16 @@ class Footer extends React.Component {
   render() {
     return (
         <div className='footer'>
-          <h1 className='footer-header'>
-            THIS IS THE HEADER
-          </h1>
+          <h1 className='footer-appname-header'>EventLite</h1>
+            <h2 className='footer-name'>
+              Learn More About Noe Jimenez
+            </h2>
+            <ul className='link-list'>
+              <a href="https://www.linkedin.com/in/noe-jimenez/" target="_blank"><i className="fa fa-linkedin-square link-fa" aria-hidden="true"></i></a>
+              <a href="https://github.com/Noejoaquin" target="_blank"><i className="fa fa-github-square link-fa" aria-hidden="true"></i></a>
+              <a href="http://www.noejimenez.com" target="_blank"><i className="fa fa-user-circle link-fa" aria-hidden="true"></i></a>
+            </ul>
+          <div className='border'></div>
         </div>
       )
   }
