b0cf2f80ec49c1acd4462d4e3e68e3a8236eea8b
Noe <noe.joaquin.james@gmail.com>
2018-01-05 14:46:13 -0500

    fix nav buttons, fix warning for guest profile

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// <button id='create-event'>Create An Event</button>
 import SearchBarContainer from '../search/search_bar_container';
 
 class NavBar extends React.Component {
@@ -18,21 +17,25 @@ class NavBar extends React.Component {
     let createEventLink;
     let landingPageLink;
     let browseEventsLink;
-
+    let profileLink;
     let searchBar = this.props.location.pathname === '/' ? <div></div> : <SearchBarContainer props={this.props}/>
-    if (this.props.currentUser){
-      let  profileLink = <Link to={`/users/${this.props.currentUser.id}`}><button id='organizer-profile-link'>Profile</button></Link>
-      if (this.props.location.pathname.includes('events') && !this.props.location.pathname.includes('events/')){
-        browseEventsLink = <button className='browse-link' id='browse-events'>Browse Events</button>
-      } else {
-        browseEventsLink = <Link to='/events'><button className='browse-link' id='browse-events'>Browse Events</button></Link>
-      }
 
-      if (this.props.location.pathname === '/'){
-        landingPageLink = <button className='header-title'>eventLite</button>
-        } else {
-          landingPageLink = <Link className='header-title' to='/'>eventLite</Link>
-          }
+    if (this.props.location.pathname.includes('events') && !this.props.location.pathname.includes('events/')){
+      browseEventsLink = <button className='browse-link' id='browse-events'>Browse Events</button>
+    } else {
+      browseEventsLink = <Link to='/events'><button className='browse-link' id='browse-events'>Browse Events</button></Link>
+    }
+    if (this.props.location.pathname === '/'){
+      landingPageLink = <button className='header-title'>eventLite</button>
+    } else {
+      landingPageLink = <Link className='header-title' to='/'>eventLite</Link>
+    }
+    if(this.props.location.pathname.includes('users')){
+      profileLink = <button id='organizer-profile-link'>Profile</button>
+    } else {
+      profileLink = <Link to={`/users/${this.props.currentUser.id}`}><button id='organizer-profile-link'>Profile</button></Link>
+    }
+    if (this.props.currentUser){
 
       if (this.props.location.pathname === '/events/new' ){
           createEventLink = <button id='create-event'>Create An Event</button>
@@ -79,16 +82,7 @@ class NavBar extends React.Component {
       </header>
       );
     } else {
-      if (this.props.location.pathname.includes('events') && !this.props.location.pathname.includes('events/')){
-        browseEventsLink = <button className='browse-link' id='browse-events'>Browse Events</button>
-      } else {
-        browseEventsLink = <Link to='/events'><button className='browse-link' id='browse-events'>Browse Events</button></Link>
-      }
-      if (this.props.location.pathname === '/'){
-        landingPageLink = <button className='header-title'>eventLite</button>
-        } else {
-          landingPageLink = <Link className='header-title' to='/'>eventLite</Link>
-          }
+
       if (this.props.location.pathname === '/events/new' ){
           createEventLink = <button id='create-event'>Create An Event</button>
       } else {
