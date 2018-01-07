ff3f529ac1607214b4e015e75f2dea3e0e17b61e
Noe <noe.joaquin.james@gmail.com>
2018-01-07 17:40:36 -0500

    adjust styling on event show page

--- frontend/components/events/event_show.jsx
+++ frontend/components/events/event_show.jsx
@@ -87,56 +87,56 @@ class EventShow extends React.Component {
       let ticketPrice = this.props.event.price === 0 ? 'Free' : '$'.concat(this.props.event.price)
 
       return (
-        <div className='whole-container-show'>
-        <div className='show-container'>
-          <div className='show-top'>
-            <div style={{backgroundImage: `url(${this.props.event.image_url})`}} className='event-image'></div>
-            <div className='event-details-show'>
-              <div className='listing-top'>
-                <time id='listing-time'>{date}</time>
-                <div id='listing-name'>{this.props.event.name}</div>
-                <div id='listing-organizer'>
-                  {this.props.event.organizer}</div>
-                {editButton}
-              </div>
-              <div className='events-price-show'>{ticketPrice}</div>
-            </div>
-          </div>
-
-          <div className='listing-panel'>
-
-              {attendanceButton}
-
-              {saveButton}
-
-          </div>
-
-            <div className='listing-cell'>
-              <div className='description'>
-                <h3 className='title description-title'>description</h3>
-                <div className='description-contents' dangerouslySetInnerHTML={{__html: this.props.event.description}}>
-              </div>
-            </div>
-              <div className='event-details'>
-                <div className='date-and-time'>
-                  <h3 className='title date-and-time'>date and time</h3>
-                  <ul className='info'>
-                    <li>{date}</li>
-                    <li>{time}</li>
-                  </ul>
+          <div className='whole-container-show'>
+          <div className='show-container'>
+            <div className='show-top'>
+              <div style={{backgroundImage: `url(${this.props.event.image_url})`}} className='event-image'></div>
+              <div className='event-details-show'>
+                <div className='listing-top'>
+                  <time id='listing-time'>{date}</time>
+                  <div id='listing-name'>{this.props.event.name}</div>
+                  <div id='listing-organizer'>
+                    {this.props.event.organizer}</div>
+                  {editButton}
                 </div>
-                <div className='location'>
-                  <h3 className='title location'>location</h3>
-                  <li className='info'>{this.props.event.location}</li>
+                <div className='events-price-show'>{ticketPrice}</div>
+              </div>
+            </div>
+
+            <div className='listing-panel'>
+
+                {attendanceButton}
+
+                {saveButton}
+
+            </div>
+
+              <div className='listing-cell'>
+                <div className='description'>
+                  <h3 className='title description-title'>description</h3>
+                  <div className='description-contents' dangerouslySetInnerHTML={{__html: this.props.event.description}}>
                 </div>
               </div>
-            </div>
-            <div className='tag-container'>
-              <h3 className='title'>TAGS</h3>
-              <button className='tag-button' onClick={this.handleTagClick}>{this.props.categories[this.props.event.category_id].name}</button>
-            </div>
+                <div className='event-details'>
+                  <div className='date-and-time'>
+                    <h3 className='title date-and-time'>date and time</h3>
+                    <ul className='info'>
+                      <li>{date}</li>
+                      <li>{time}</li>
+                    </ul>
+                  </div>
+                  <div className='location'>
+                    <h3 className='title location'>location</h3>
+                    <li className='info'>{this.props.event.location}</li>
+                  </div>
+                </div>
+              </div>
+              <div className='tag-container'>
+                <h3 className='title'>TAGS</h3>
+                <button className='tag-button' onClick={this.handleTagClick}>{this.props.categories[this.props.event.category_id].name}</button>
+              </div>
+           </div>
          </div>
-       </div>
       )
 
     }
