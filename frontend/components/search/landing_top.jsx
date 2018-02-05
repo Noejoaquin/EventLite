f3b4cfc91264f60700da64bb1339c5f488232b93
Noe <noe.joaquin.james@gmail.com>
2018-02-05 17:24:42 -0500

    start rotating landing images

--- frontend/components/search/landing_top.jsx
+++ frontend/components/search/landing_top.jsx
@@ -6,13 +6,32 @@ import SearchBarContainer from './search_bar_container';
 class LandingTop extends React.Component {
   constructor(props){
     super(props);
+    this.handleChangeOfPhoto = this.handleChangeOfPhoto.bind(this);
   }
+
+  handleChangeOfPhoto() {
+      setInterval(() => {
+        let image = document.getElementsByClassName('LandingImage')[0]
+        if (image.classList.contains('one')){
+          image.classList.remove('one')
+          image.classList.add('two')
+        } else if (image.classList.contains('two')) {
+          image.classList.remove('two')
+          image.classList.add('three')
+        } else {
+          image.classList.remove('three')
+          image.classList.add('one')
+        }
+      }, 5000)
+  }
+
 
 
   render(){
     return (
       <div className='root-page'>
-      <img className='LandingImage'></img>
+        {this.handleChangeOfPhoto()}
+      <img className='LandingImage one'></img>
           <div className='meta-search-card'>
             <h1>Find your next Experience</h1>
               <SearchBarContainer props={this.props}/>
