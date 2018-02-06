763fc1ea6783b8c337220d6f3bed56861da166b5
Noe <noe.joaquin.james@gmail.com>
2018-02-06 11:46:49 -0500

    make sure setInterval is present when component mounts, and disables when it dismounts

--- frontend/components/search/landing_top.jsx
+++ frontend/components/search/landing_top.jsx
@@ -1,45 +1,55 @@
-import React from 'react';
-import SearchCard  from './search_card';
-import EventIndexContainer from '../events/event_index_container';
-import SearchBarContainer from './search_bar_container';
+import React from "react";
+import SearchCard from "./search_card";
+import EventIndexContainer from "../events/event_index_container";
+import SearchBarContainer from "./search_bar_container";
 
 class LandingTop extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.handleChangeOfPhoto = this.handleChangeOfPhoto.bind(this);
   }
 
-  handleChangeOfPhoto() {
-      setInterval(() => {
-        let image = document.getElementsByClassName('LandingImage')[0]
-        if (image.classList.contains('one')){
-          image.classList.remove('one')
-          image.classList.add('two')
-        } else if (image.classList.contains('two')) {
-          image.classList.remove('two')
-          image.classList.add('three')
-        } else {
-          image.classList.remove('three')
-          image.classList.add('one')
-        }
-      }, 5000)
+  componentDidMount() {
+    this.handleChangeOfPhoto();
   }
 
+  componentWillUnmount() {
+    clearInterval(this.imageInterval);
+  }
 
+  handleChangeOfPhoto() {
+    this.imageInterval = setInterval(() => {
+      let image = document.getElementsByClassName("LandingImage")[0];
+      if (image.classList.contains("one")) {
+        image.classList.remove("one");
+        image.classList.add("two");
+      } else if (image.classList.contains("two")) {
+        image.classList.remove("two");
+        image.classList.add("three");
+      } else if (image.classList.contains("three")) {
+        image.classList.remove("three");
+        image.classList.add("four");
+      } else {
+        image.classList.remove("four");
+        image.classList.add("one");
+      }
+    }, 5000);
+  }
 
-  render(){
+  // {this.handleChangeOfPhoto()}
+
+  render() {
     return (
-      <div className='root-page'>
-        {this.handleChangeOfPhoto()}
-      <img className='LandingImage one'></img>
-          <div className='meta-search-card'>
-            <h1>Find your next Experience</h1>
-              <SearchBarContainer props={this.props}/>
-          </div>
-        <EventIndexContainer props={this.props}/>
+      <div className="root-page">
+        <img className="LandingImage one" />
+        <div className="meta-search-card">
+          <h1>Find your next Experience</h1>
+          <SearchBarContainer props={this.props} />
+        </div>
+        <EventIndexContainer props={this.props} />
       </div>
-    )
+    );
   }
 }
 
-export default LandingTop
+export default LandingTop;
